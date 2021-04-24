67b3c137fa0a03557313a146336d9374bc71e5a1
Issac To <issacto606@gmail.com>
2021-04-24 18:25:07 +0100

    add weights test

--- test/test.js
+++ test/test.js
@@ -4,12 +4,12 @@ var NeuralNetwork = require('../index').NeuralNetwork;
 test('Neural Network Answer Testing', () => {
     trainInputs =[[1,0,0],[1,1,1],[1,0,1],[1,1,0],[2,1,0],[1,1,0]]
     trainOutputs =[0,1,1,0,0,0]
-    testInputs =[[1,0,0]]
+    predictInput =[[1,0,0]]
     var network = new NeuralNetwork([3,3,3,1])
     var epoch = 100
     var learningRate =0.1
     network.train(trainInputs,trainOutputs, epoch,learningRate)
-    var ans = network.predict(testInputs)
+    var ans = network.predict(predictInput)
     expect(Number.isFinite(ans)).toBe(true);
     expect(-1<=ans).toBe(true);
     expect(ans<=1).toBe(true);
@@ -18,11 +18,22 @@ test('Neural Network Answer Testing', () => {
 test('Neural Network Error Testing', () => {
     trainInputs =[[1,0,0],[1,1,1],[1,0,1],[1,1,0],[2,1,0],[1,1,0]]
     trainOutputs =[0,1,1,0,0,0]
-    testInputs =[[1,0,0]]
     var network = new NeuralNetwork([3,3,3,1])
     var epoch = 100
     var learningRate =0.1
     network.train(trainInputs,trainOutputs, epoch,learningRate)
     var errors = network.getErrors()
     expect(Array.isArray(errors)).toBe(true);
+});
+
+
+test('Neural Network Error Testing', () => {
+    trainInputs =[[1,0,0],[1,1,1],[1,0,1],[1,1,0],[2,1,0],[1,1,0]]
+    trainOutputs =[0,1,1,0,0,0]
+    var network = new NeuralNetwork([3,3,3,1])
+    var epoch = 100
+    var learningRate =0.1
+    network.train(trainInputs,trainOutputs, epoch,learningRate)
+    var weights = network.getWeights()
+    expect(Array.isArray(weights)).toBe(true);
 });
