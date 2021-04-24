43f8130b267251d8b4de752d44039472013b02db
Issac To <issacto606@gmail.com>
2021-04-24 18:14:07 +0100

    add error testing

--- test/test.js
+++ test/test.js
@@ -1,16 +1,30 @@
 var NeuralNetwork = require('../index').NeuralNetwork;
 
 
-test('Neural Network testing', () => {
+test('Neural Network Answer Testing', () => {
     trainInputs =[[1,0,0],[1,1,1],[1,0,1],[1,1,0],[2,1,0],[1,1,0]]
     trainOutputs =[0,1,1,0,0,0]
     testInputs =[[1,0,0]]
-    var x = new NeuralNetwork([3,3,3,1])
+    var network = new NeuralNetwork([3,3,3,1])
     var epoch = 100
     var learningRate =0.1
-    x.train(trainInputs,trainOutputs, epoch,learningRate)
-    var ans = x.predict(testInputs)
+    network.train(trainInputs,trainOutputs, epoch,learningRate)
+    //test answer
+    var ans = network.predict(testInputs)
     expect(Number.isFinite(ans)).toBe(true);
     expect(-1<=ans).toBe(true);
     expect(ans<=1).toBe(true);
+});
+
+test('Neural Network Errors Testing', () => {
+    trainInputs =[[1,0,0],[1,1,1],[1,0,1],[1,1,0],[2,1,0],[1,1,0]]
+    trainOutputs =[0,1,1,0,0,0]
+    testInputs =[[1,0,0]]
+    var network = new NeuralNetwork([3,3,3,1])
+    var epoch = 100
+    var learningRate =0.1
+    network.train(trainInputs,trainOutputs, epoch,learningRate)
+    //test errors
+    var errors = network.getErrors()
+    expect(Array.isArray(errors)).toBe(true);
 });
